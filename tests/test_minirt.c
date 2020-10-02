814cebc2639dc0a569189a38200e1fa6a203b2e7
vinicius111andrade <vinicius111andrade@gmail.com>
2020-10-02 18:48:13 -0300

    first functions

---
+++ tests/test_minirt.c
@@ -0,0 +1,11 @@
+int		test_minirt()
+{
+	printf("\nSTART of minirt.c tests\n\n")
+
+	if (minirt() == 0)
+		printf("Return correto.\n")
+	else
+		printf("Return errado.\n")
+	
+	printf("\nEND of minirt.c tests\n\n")
+}
