d43e6a48167c8f3232629012a6194cd7ab6ad1b7
vinicius111andrade <vinicius111andrade@gmail.com>
2020-10-29 16:38:36 -0300

    reorganizacao

---
+++ my_libs/vectors/libvectors.h
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libvectors.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-melo <vde-melo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/28 14:59:11 by vde-melo          #+#    #+#             */
+/*   Updated: 2020/10/28 17:19:52 by vde-melo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBVECTORS_H
+# define LIBVECTORS_H
+
+# include <math.h>
+
+typedef struct	s_vector
+{
+	double		x;
+	double		y;
+	double		z;
+}				t_vector;
+
+t_vector		v_define(double x, double y, double z);
+t_vector		v_invert_direction(t_vector vector);
+t_vector		v_add(t_vector a, t_vector b);
+t_vector		v_substract(t_vector a, t_vector b);
+double			v_dotproduct(t_vector a, t_vector b);
+
+t_vector		v_crossproduct(t_vector a, t_vector b);
+double			v_length(t_vector v);
+t_vector		v_normalize(t_vector p);
+double			v_cos(t_vector a, t_vector b);
+double			v_sin(t_vector a, t_vector b);
+
+t_vector		v_scale(double scalar, t_vector p);
+double			v_distance(t_vector p1, t_vector p2);
+t_vector		x_axis_rotation(t_vector vec, double deg_angle);
+t_vector		y_axis_rotation(t_vector vec, double deg_angle);
+t_vector		z_axis_rotation(t_vector vec, double deg_angle);
+
+#endif
---
+++ my_libs/vectors/vector_operations_0.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   vector_operations_0.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-melo <vde-melo@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/10/28 15:23:33 by vde-melo          #+#    #+#             */
+/*   Updated: 2020/10/28 17:19:54 by vde-melo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libvectors.h"
+
+t_vector		v_define(double x, double y, double z)
+{
+	t_vector	vector;
+
+	vector.x = x;
+	vector.y = y;
+	vector.z = z;
+	return (vector);
+}
+
+t_vector		v_invert_direction(t_vector vector)
+{
+	t_vector	inversion;
+
+	inversion.x = -(vector.x);
+	inversion.y = -(vector.y);
+	inversion.z = -(vector.z);
+	return(inversion);
+}
+
+t_vector		v_add(t_vector a, t_vector b)
+{
+	t_vector	sum;
+
+	sum.x = a.x + b.x;
+	sum.y = a.y + b.y;
+	sum.z = a.z + b.z;
+	return (sum);
+}
+
+t_vector		v_substract(t_vector a, t_vector b)
+{
+	return (v_add(a, v_invert_direction(b)));
+}
+
+double		v_dotproduct(t_vector a, t_vector b)
+{
+	return (a.x * b.x + a.y * b.y + a.z * b.z);
+}
+
